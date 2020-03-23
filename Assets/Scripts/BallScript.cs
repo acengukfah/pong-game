c27492151c8e482aee814047634d344904f221a9
acengukfah <acengukfah@gmail.com>
2020-03-23 14:11:33 +0700

    Change rigidbody refference

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallScript : MonoBehaviour
-{
+public class BallScript : MonoBehaviour {
     public int speed = 20;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
-    void Start()
-    {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(1, 1) * speed;
+    void Start () {
+        rb.velocity = new Vector2 (1, 1) * speed;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(1, 1) * speed;
+    private void OnTriggerEnter2D (Collider2D collision) {
+        rb.velocity = new Vector2 (1, 1) * speed;
     }
 }
