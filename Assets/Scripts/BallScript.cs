8880c50575985eaff9f38eb9b694c1bd834d2941
acengukfah <acengukfah@gmail.com>
2020-04-07 22:51:36 +0800

    animation api done

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -6,16 +6,25 @@ public class BallScript : MonoBehaviour
 {
     public int speed = 20;
     public Rigidbody2D rb;
+    public Animator animtr;
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = new Vector2(-1, -1) * speed;
+        animtr.SetBool("isMove", true);
+    }
+    void FixedUpdate()
+    {
+        if (rb.velocity.x > 0)
+        {
+            rb.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            rb.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        rb.velocity = new Vector2(-1, -1) * speed;
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.name == "WallVerticalLeft" || collision.collider.name == "WallVerticalRight")
@@ -26,8 +35,10 @@ public class BallScript : MonoBehaviour
     IEnumerator jeda()
     {
         rb.velocity = Vector2.zero;
+        animtr.SetBool("isMove", false);
         rb.GetComponent<Transform>().position = Vector2.zero;
         yield return new WaitForSeconds(1);
         rb.velocity = new Vector2(-1, -1) * speed;
+        animtr.SetBool("isMove", true);
     }
 }
