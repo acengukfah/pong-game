9bde7e690cb3d0dae77340e7a7548be83d13b3d3
acengukfah <acengukfah@gmail.com>
2020-04-07 21:52:18 +0800

    timer and jeda done

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallScript : MonoBehaviour {
+public class BallScript : MonoBehaviour
+{
     public int speed = 20;
     public Rigidbody2D rb;
     // Start is called before the first frame update
-    void Start () {
-        rb.velocity = new Vector2 (1, 1) * speed;
+    void Start()
+    {
+        rb.velocity = new Vector2(-1, -1) * speed;
     }
 
-    private void OnTriggerEnter2D (Collider2D collision) {
-        rb.velocity = new Vector2 (1, 1) * speed;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rb.velocity = new Vector2(-1, -1) * speed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "WallVerticalLeft" || collision.collider.name == "WallVerticalRight")
+        {
+            StartCoroutine(jeda());
+        }
+    }
+    IEnumerator jeda()
+    {
+        rb.velocity = Vector2.zero;
+        rb.GetComponent<Transform>().position = Vector2.zero;
+        yield return new WaitForSeconds(1);
+        rb.velocity = new Vector2(-1, -1) * speed;
     }
 }
